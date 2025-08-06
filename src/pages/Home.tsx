5866ae4933a22f358e18b58b03b393332f2ed1fa
Hannah Q Kim <hk2684@nyu.edu>
2025-08-05 23:45:20 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -40,7 +40,7 @@ const Home = () => {
         <div className="flex gap-4">
           <Button 
             className="bg-white text-black border border-gray-300 px-8 py-3 rounded-full font-bold shadow-md transition-all duration-200 transform hover:bg-black hover:text-white hover:border-black focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2"
-            onClick={() => window.open('https://drive.google.com/file/d/1Y0TeD-cfV44QuEq8IUHFei68kGwmsmgO/view?usp=sharing', '_blank')}
+            onClick={() => window.open('https://drive.google.com/file/d/1JLHK7NUBzMJeikl654HatqxQFtBPU3Ui/view?usp=sharing', '_blank')}
           >
             <Download className="w-5 h-5 mr-2" />
             Resume
